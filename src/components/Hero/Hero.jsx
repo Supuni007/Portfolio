82d2ed575a182a64997e07dcf3eba25fe33540ee
Supuni Rathnayake <supunirathnayake007@gmail.com>
2024-02-22 03:27:42 +0530

    Create hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+    return (
+        <section className={styles.container}>
+            <div className={styles.content}>
+                <h1 className={styles.title}>Hi, I'm Supuni</h1>
+                <p className={styles.description}>
+                    I'm a graduate student with the knowledge of React, Python and Flutter mobile development. Reach out if you'd like to know more!
+                </p>
+                <a href="mailto:supunirathnayake007@gmail.com" className={styles.contactBtn}>Contact Me</a>
+            </div>
+            <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" classNAme={styles.heroImg}></img>
+            <div className={StyleSheet.topBlur}/>
+            <div className={StyleSheet.bottomBlur}/>
+        </section>
+    );
+};
