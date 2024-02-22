e4158da415987151b0024d9c4f169af7bd882bc0
Supuni Rathnayake <supunirathnayake007@gmail.com>
2024-02-22 19:42:38 +0530

    Make hero section responsive

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,13 +9,13 @@ export const Hero = () => {
             <div className={styles.content}>
                 <h1 className={styles.title}>Hi, I'm Supuni</h1>
                 <p className={styles.description}>
-                    I'm a graduate student with the knowledge of React, Python and Flutter mobile development. Reach out if you'd like to know more!
+                    I'm a graduate with the knowledge of React, Python and Flutter mobile development. Reach out if you'd like to know more!
                 </p>
                 <a href="mailto:supunirathnayake007@gmail.com" className={styles.contactBtn}>Contact Me</a>
             </div>
-            <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" classNAme={styles.heroImg}></img>
-            <div className={StyleSheet.topBlur}/>
-            <div className={StyleSheet.bottomBlur}/>
+            <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" className={styles.heroImg}></img>
+            <div className={styles.topBlur}></div>
+            <div className={styles.bottomBlur}></div>
         </section>
     );
 };
